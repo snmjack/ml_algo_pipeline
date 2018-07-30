10a0a4f34a54bd8164dd344828832fe9a8c9f30c
Swapnil Mahajan <31958717+snmjack@users.noreply.github.com>
2018-07-30 15:03:38 +0530

    Create ml_algo_pipeline.py

---
+++ ml_algo_pipeline.py
@@ -0,0 +1,7 @@
+# Imports
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import test_train_split
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor
